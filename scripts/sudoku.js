3d12058ed2fcd6634febd5c7fdc59fcaf47970fd
rashaad367 <92348662+rashaad367@users.noreply.github.com>
2022-12-23 18:55:49 -0500

    Update sudoku.js

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -228,36 +228,48 @@ $(document).ready(function() {
         if (parts.length == 2) return parts.pop().split(";").shift();
     }
 
-     if (getCookie("#f5f5f5") == "enabled") {
-        $("#board").css("background-color","#f5f5f5");
-        $(".tile-start").css("background-color","#f5f5f5");
-    } else if (getCookie("#d3d3d3") == "enabled") {
-        $("#board").css("background-color","#d3d3d3");
-        $(".tile-start").css("background-color","#d3d3d3");
-    } else if (getCookie("#ffebcd") == "enabled") {
-        $("#board").css("background-color","#ffebcd");
-        $(".tile-start").css("background-color","#ffebcd");
-    } else if (getCookie("#778899") == "enabled") {
-        $("#board").css("background-color","#778899");
-        $(".tile-start").css("background-color","#778899");
-    } else if (getCookie("#c0c0c0") == "enabled") {
-        $("#board").css("background-color","#c0c0c0");
-        $(".tile-start").css("background-color","#c0c0c0");
-        console.log($("#board"));
-    } else if (getCookie("#f08080") == "enabled") {
-        $("#board").css("background-color","#f08080");
-        $(".tile-start").css("background-color","#f08080");
-    } else if (getCookie("#fafad2") == "enabled") {
-        $("#board").css("background-color","#fafad2");
-        $(".tile-start").css("background-color","#fafad2");
-    } else if (getCookie("#90ee90") == "enabled") {
-        $("#board").css("background-color","#90ee90");
-        $(".tile-start").css("background-color","#90ee90");
-    } else if (getCookie("#add8e6") == "enabled") {
-        $("#board").css("background-color","#add8e6");
-        $(".tile-start").css("background-color","#add8e6");
-    } else if (getCookie("#deb887") == "enabled") {
-        $("#board").css("background-color","#deb887");
-        $(".tile-start").css("background-color","#deb887");
+     $(document).ready(function() {
+    // cookie getter
+    // if the name is found within all currently set cookies on the site
+    // then the cookie is retrieved
+    function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
     }
+
+    setTimeout(function() {
+        if (getCookie("#f5f5f5") == "enabled") {
+            $("#board").css("background-color","#f5f5f5");
+            $(".tile-start").css("background-color","#f5f5f5");
+        } else if (getCookie("#d3d3d3") == "enabled") {
+            $("#board").css("background-color","#d3d3d3");
+            $(".tile-start").css("background-color","#d3d3d3");
+        } else if (getCookie("#ffebcd") == "enabled") {
+            $("#board").css("background-color","#ffebcd");
+            $(".tile-start").css("background-color","#ffebcd");
+        } else if (getCookie("#778899") == "enabled") {
+            $("#board").css("background-color","#778899");
+            $(".tile-start").css("background-color","#778899");
+        } else if (getCookie("#c0c0c0") == "enabled") {
+            $("#board").css("background-color","#c0c0c0");
+            $(".tile-start").css("background-color","#c0c0c0");
+            console.log($("#board"));
+        } else if (getCookie("#f08080") == "enabled") {
+            $("#board").css("background-color","#f08080");
+            $(".tile-start").css("background-color","#f08080");
+        } else if (getCookie("#fafad2") == "enabled") {
+            $("#board").css("background-color","#fafad2");
+            $(".tile-start").css("background-color","#fafad2");
+        } else if (getCookie("#90ee90") == "enabled") {
+            $("#board").css("background-color","#90ee90");
+            $(".tile-start").css("background-color","#90ee90");
+        } else if (getCookie("#add8e6") == "enabled") {
+            $("#board").css("background-color","#add8e6");
+            $(".tile-start").css("background-color","#add8e6");
+        } else if (getCookie("#deb887") == "enabled") {
+            $("#board").css("background-color","#deb887");
+            $(".tile-start").css("background-color","#deb887");
+        }    
+    }, 2000);
 });
