ff1cf2ed12dca20fe6b6983c1c0a8e58638a2f0b
rashaad367 <92348662+rashaad367@users.noreply.github.com>
2022-12-25 20:05:47 -0500

    Update sudoku-menu.js

--- scripts/sudoku-menu.js
+++ scripts/sudoku-menu.js
@@ -20,6 +20,12 @@ $(document).ready(function() {
         $(".diff").removeClass("selected");
         // adds class to difficulty button just clicked on
         $(this).addClass("selected");
+        // if difficulty is changed then cookies for difficulty resets for a new one
+        if (document.cookie != null) {
+            deleteCookie("Easy");
+            deleteCookie("Medium");
+            deleteCookie("Hard");
+        }
         setCookie(this.innerText, "enabled", expDate);
         console.log(this.innerText) // REMOVE EVENTUALLY
     });
@@ -27,6 +33,19 @@ $(document).ready(function() {
     // change color of board using cookies
     $(".chg-color-btn").click(function() {
         var color = $("#custom").spectrum("get");
+        // if difficulty is changed then cookies for difficulty resets for a new one
+        if (document.cookie != null) {
+            deleteCookie("#f5f5f5");
+            deleteCookie("#d3d3d3");
+            deleteCookie("#ffebcd");
+            deleteCookie("#778899");
+            deleteCookie("#c0c0c0");
+            deleteCookie("#f08080");
+            deleteCookie("#fafad2");
+            deleteCookie("#90ee90");
+            deleteCookie("#add8e6");
+            deleteCookie("#deb887")
+        }
         setCookie(color.toHexString(), "enabled", expDate);
         console.log(color.toHexString()); // REMOVE EVENTUALLY
     });
@@ -52,4 +71,14 @@ function deleteAllCookies() {
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+}
+
+function deleteCookie(name) {
+    // set cookie's value to an empty string
+    document.cookie = name + "=;";
+
+    // set the cookie's expiration date to a date in the past
+    var date = new Date();
+    date.setTime(date.getTime() - 1);
+    document.cookie = name + "=; expires=" + date.toGMTString();
 }
