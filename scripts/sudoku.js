8c8880ffdd1f115af3ececb828a5f04384b37e20
rashaad367 <92348662+rashaad367@users.noreply.github.com>
2022-12-25 20:18:07 -0500

    Update sudoku.js

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -72,53 +72,48 @@ window.onload = function() {
 
 
 // using cookies to change board color and difficulty
-$(document).ready(function() {
-    $(document).one("mousemove", function() { // every time page shows, an event is triggered to check cookies
-        // sets up board color
-        if (getCookie("#f5f5f5") == "enabled") {
-            $("#board").css("background-color","#f5f5f5");
-            $(".tile-start").css("background-color","#f5f5f5");
-            $(".number").css("background-color", "#f5f5f5");
-        } else if (getCookie("#d3d3d3") == "enabled") {
-            $("#board").css("background-color","#d3d3d3");
-            $(".tile-start").css("background-color","#d3d3d3");
-            $(".number").css("background-color", "#d3d3d3");
-        } else if (getCookie("#ffebcd") == "enabled") {
-            $("#board").css("background-color","#ffebcd");
-            $(".tile-start").css("background-color","#ffebcd");
-            $(".number").css("background-color", "#ffebcd");
-        } else if (getCookie("#778899") == "enabled") {
-            $("#board").css("background-color","#778899");
-            $(".tile-start").css("background-color","#778899");
-            $(".number").css("background-color", "#778899");
-        } else if (getCookie("#c0c0c0") == "enabled") {
-            $("#board").css("background-color","#c0c0c0");
-            $(".tile-start").css("background-color","#c0c0c0");
-            $(".number").css("background-color", "#c0c0c0");
-            console.log($("#board"));
-        } else if (getCookie("#f08080") == "enabled") {
-            $("#board").css("background-color","#f08080");
-            $(".tile-start").css("background-color","#f08080");
-            $(".number").css("background-color", "#f08080");
-        } else if (getCookie("#fafad2") == "enabled") {
-            $("#board").css("background-color","#fafad2");
-            $(".tile-start").css("background-color","#fafad2");
-            $(".number").css("background-color", "#fafad2");
-        } else if (getCookie("#90ee90") == "enabled") {
-            $("#board").css("background-color","#90ee90");
-            $(".tile-start").css("background-color","#90ee90");
-            $(".number").css("background-color", "#90ee90");
-        } else if (getCookie("#add8e6") == "enabled") {
-            $("#board").css("background-color","#add8e6");
-            $(".tile-start").css("background-color","#add8e6");
-            $(".number").css("background-color", "#add8e6");
-        } else if (getCookie("#deb887") == "enabled") {
-            $("#board").css("background-color","#deb887");
-            $(".tile-start").css("background-color","#deb887");
-            $(".number").css("background-color", "#deb887");
-        }
-    });
-});
+if (getCookie("#f5f5f5") == "enabled") {
+    $("#board").css("background-color","#f5f5f5");
+    $(".tile-start").css("background-color","#f5f5f5");
+    $(".number").css("background-color", "#f5f5f5");
+} else if (getCookie("#d3d3d3") == "enabled") {
+    $("#board").css("background-color","#d3d3d3");
+    $(".tile-start").css("background-color","#d3d3d3");
+    $(".number").css("background-color", "#d3d3d3");
+} else if (getCookie("#ffebcd") == "enabled") {
+    $("#board").css("background-color","#ffebcd");
+    $(".tile-start").css("background-color","#ffebcd");
+    $(".number").css("background-color", "#ffebcd");
+} else if (getCookie("#778899") == "enabled") {
+    $("#board").css("background-color","#778899");
+    $(".tile-start").css("background-color","#778899");
+    $(".number").css("background-color", "#778899");
+} else if (getCookie("#c0c0c0") == "enabled") {
+    $("#board").css("background-color","#c0c0c0");
+    $(".tile-start").css("background-color","#c0c0c0");
+    $(".number").css("background-color", "#c0c0c0");
+    console.log($("#board"));
+} else if (getCookie("#f08080") == "enabled") {
+    $("#board").css("background-color","#f08080");
+    $(".tile-start").css("background-color","#f08080");
+    $(".number").css("background-color", "#f08080");
+} else if (getCookie("#fafad2") == "enabled") {
+    $("#board").css("background-color","#fafad2");
+    $(".tile-start").css("background-color","#fafad2");
+    $(".number").css("background-color", "#fafad2");
+} else if (getCookie("#90ee90") == "enabled") {
+    $("#board").css("background-color","#90ee90");
+    $(".tile-start").css("background-color","#90ee90");
+    $(".number").css("background-color", "#90ee90");
+} else if (getCookie("#add8e6") == "enabled") {
+    $("#board").css("background-color","#add8e6");
+    $(".tile-start").css("background-color","#add8e6");
+    $(".number").css("background-color", "#add8e6");
+} else if (getCookie("#deb887") == "enabled") {
+    $("#board").css("background-color","#deb887");
+    $(".tile-start").css("background-color","#deb887");
+    $(".number").css("background-color", "#deb887");
+}
 
 // sets up sudoku board and digits
 function setGame() { 
