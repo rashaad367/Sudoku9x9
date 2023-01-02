20de753f31478433bbed006c728934b6e60715a2
rashaad367 <92348662+rashaad367@users.noreply.github.com>
2023-01-02 16:03:51 -0500

    Update sudoku.js

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -176,7 +176,9 @@ for (var d = 1; d <= 9; d++) {
     var numCount = flatBoard.reduce((acc,curr) => (curr == d.toString() ? acc + 1 : acc), 0);
     // if number is found 9 times, its number selector disappears
     if (numCount == 9) {
-        document.getElementById(d).remove();
+        if (String(d) != null) {
+            document.getElementById(String(d)).remove();
+        }
     }
 }
 
